eaedb0b1731fd3f19966bbbeb893e3ee483b3b3e
Rohit Yadav <rohithiwal13@gmail.com>
2025-11-18 16:39:34 +0530

    generated the qr

---
+++ generate_qr.py
@@ -0,0 +1,26 @@
+import qrcode
+
+# --- CONFIGURATION ---
+# Replace this with your AWS Public IP address
+# Make sure to include http:// and :8000
+WEBSITE_URL = "http://54.167.158.254:8000" 
+
+# --- GENERATE QR CODE ---
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data(WEBSITE_URL)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+# --- SAVE IMAGE ---
+filename = "ar_translator_qr.png"
+img.save(filename)
+
+print(f"Success! QR code saved as '{filename}'.")
+print(f"Scan it to go to: {WEBSITE_URL}")
